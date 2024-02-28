9ce1dac6becb45214c0567dcdae459320ef2f1e3
bheesetti anand <anandbheesetti@gmail.com>
2024-02-28 21:47:33 +0530

    folder structure updated

---
+++ template.py
@@ -0,0 +1,47 @@
+# this template.py file is used to create the entire project worlflow that is it creates all folders
+
+
+import os   # to interact with the system os
+from pathlib import Path  # to take care of the slahes present in the path based on the environments
+
+list_of_files=[
+    ".github/workflows/.gitkeep",
+    "src/__init__.py",
+    "src/components/__init__.py",
+    "src/components/data_ingestion.py",
+    "src/components/data_transformation.py",
+    "src/components/model_trainer.py",
+    "src/components/model_evaluation.py",
+    "src/pipeline/__init__.py",
+    "src/pipeline/training_pipeline.py",
+    "src/pipeline/prediction_pipeline.py",
+    "src/utils/__init__.py",
+    "src/utils/utils.py",
+    "src/logger/logging.py",
+    "src/exceptions/exceptions.py",
+    
+    "src/unit/__init__.py",
+    "src/integration/__init__.py",
+    "init_setup.sh",
+    "requirements.txt",
+    "requirements_dev/txt",
+    "setup.py",
+    "setup.cfg",
+    "pyproject.toml",
+    "tox.ini",
+    "experiment/experiments.ipynb",
+    
+    
+    
+]
+
+# now to create all the above structure in the project workflow
+
+for filepath in list_of_files:
+    filepath=Path(filepath)
+    filedir,filename=os.path.split(filepath)
+    if filedir!="":
+        os.makedirs(filedir,exist_ok=True)
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath)==0):
+        with open(filepath,"w") as f:
+            pass
